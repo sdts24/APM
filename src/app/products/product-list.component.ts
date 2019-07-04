d14443d80b802816c31b2c9cc12f1319f193e333
sdts24 <sdts24@gmail.com>
2019-07-04 07:48:17 -0400

    Cambio de Producto

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,16 +11,16 @@ export class ProductListComponent
     showImage: boolean = false;
     products: any[]  = 
     [ 
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-          },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2016",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+      },
           {
             "productId": 2,
             "productName": "Garden Cart",
