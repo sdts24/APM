830f0ea15b330859ade41e618d73be454935b655
sdts24 <sdts24@gmail.com>
2019-07-17 16:36:55 -0400

    Complete Module 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,16 @@ import { Component } from "@angular/core";
   selector: 'pm-root',
   template: 
   `
-  <div>
-  
-  <h1>{{pageTitle}}</h1>
-  <pm-product></pm-product>  
-  </div>
-  
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+      <a class='navbar-brand'> {{pageTitle}}</a>
+      <ul class ='nav nav-pills'>
+        <li><a class='nav-link' [routerLink] = "['/welcome']"> Home </a> </li>
+        <li><a class='nav-link' [routerLink] = "['/products']"> Product </a> </li>
+      </ul>
+    </nav>
+    <div class='container'>
+      <router-outlet></router-outlet>
+    </div>
   `
 })
 
