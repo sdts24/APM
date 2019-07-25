9514012135eafdf7f504f6d09273707bccdde0b1
sdts24 <sdts24@gmail.com>
2019-07-25 12:38:22 -0400

    Complete Module 12

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import{ ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,7 +11,26 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
-  ngOnInit() {  }
+  ngOnInit() {  
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2016",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "https://www.clasohlson.com/medias/sys_master/9597265281054.jpg"
+      
+    }
+  }
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
